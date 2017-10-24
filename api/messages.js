288cb657463ddd291014302cbd7dd8af94edf774
DEADBLACKCLOVER <deadblackclover@protonmail.com>
2017-10-24 22:36:17 +0700

    Add API messages

---
+++ api/messages.js
@@ -0,0 +1,22 @@
+const userdb = require('../libs/userdb');
+const express = require('express');
+const logger = require('../libs/logger');
+
+const router = express.Router();
+
+router.post("/", (req, res) => {
+  if (!req.body) return res.sendStatus(400);
+  if(req.session.authorized){
+    let to = req.body.to;
+    userdb.find({to: to}, function (err, docs) {
+      if(err){logger.save('dbfind',err);}
+      if(docs[0] != undefined){
+        res.send(true);
+      }else{
+        res.send(false);
+      }
+    })
+  }
+})
+
+module.exports = router;
