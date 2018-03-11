0259acffb4af6b0a1ad4a0ae3433be32e87b1f7d
DEADBLACKCLOVER <deadblackclover@protonmail.com>
2018-03-11 18:20:27 +0700

    New data structure

--- libs/authentication.js
+++ libs/authentication.js
@@ -1,41 +1,68 @@
-const db = require('../libs/db');
-const mail = require('../libs/mail');
-const logger = require('../libs/logger');
-const atoken = require('atoken');
+const db = require('../libs/db')
+const mail = require('../libs/mail')
+const logger = require('../libs/logger')
+const atoken = require('atoken')
 
-let Atoken = new atoken(16);
+let Atoken = new atoken(16)
 
 let authenticationUser = (email) => {
   return new Promise((resolve, reject) => {
     db.users.find({email: email}, function(err, docs) {
       if (err) {
-        logger.save('dbfind', err);
+        logger.save('dbfind', err)
       }
       if (docs[0] != undefined) {
         // Authentication user
-        let data = docs[0];
-        let token = Atoken.generate();
-        data.token = token;
+        let data = docs[0]
+        let token = Atoken.generate()
+        data.token = token
         db.users.update({email: email}, data, {}, function(err, res) {});
-        let message = `You token:${token}`;
-        let mailPromise = mail.send(email, 'Ames', message);
+        let message = `You token:${token}`
+        let mailPromise = mail.send(email, 'Ames', message)
         mailPromise.then((result) => {
-          resolve(result);
+          resolve(result)
         });
       } else {
         // Create new user
-        let token = Atoken.generate();
-        db.users.insert({email: email, token: token, username: 'User', photo: null, contacts: ['admin@1', 'admin@2', 'admin@3']});
-        let message = `You token:${token}`;
-        let mailPromise = mail.send(email, 'Ames', message);
+        let token = Atoken.generate()
+        let url = 'localhost'
+        let uid = 'sdghd7sgsdgsyd'
+        //
+        // Data structure
+        //
+        // Uid
+        // Username
+        // Url host
+        // First Name
+        // Last Name
+        // Email adress
+        // Photo
+        // Token for auth
+        // Open rsa key
+        // Close rsa key
+        //        
+        let message = `You token:${token}`
+        let mailPromise = mail.send(email, 'Ames', message)
         mailPromise.then((result) => {
-          resolve(result);
-        });
+        	db.users.insert({ 
+        		uid: uid, 
+        		username: '', 
+        		url: url, 
+        		fname: '', 
+        		lname: '', 
+        		email: email, 
+        		photo: null, 
+        		token: token, 
+        		okey: '',
+        		ckey: ''
+        	})        	
+        	resolve(result)
+        })
       }
-    });
-  });
-};
+    })
+  })
+}
 
 module.exports = {
   authenticationUser: authenticationUser,
-};
+}
