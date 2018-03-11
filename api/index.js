b58aaaa0d5518fd58a6b4a1a23440409bec0c6e9
DEADBLACKCLOVER <deadblackclover@protonmail.com>
2018-03-11 19:15:42 +0700

    Add webfinger

--- api/index.js
+++ api/index.js
@@ -1,27 +1,29 @@
-const express = require('express');
+const express = require('express')
 
-const sign = require('./sign');
-const profile = require('./profile');
-const contacts = require('./contacts');
-const send = require('./send');
-const messages = require('./messages');
+const sign = require('./sign')
+const profile = require('./profile')
+const contacts = require('./contacts')
+const send = require('./send')
+const messages = require('./messages')
+const webfinger = require('./webfinger')
 
-const router = express.Router();
-let app = express();
+const router = express.Router()
+let app = express()
 
 router.use((req, res, next) => {
-  Object.setPrototypeOf(req, app.request);
-  Object.setPrototypeOf(res, app.response);
-  req.res = res;
-  res.req = req;
-  next();
+  Object.setPrototypeOf(req, app.request)
+  Object.setPrototypeOf(res, app.response)
+  req.res = res
+  res.req = req
+  next()
 })
 
-router.use('/api/sign', sign);
-router.use('/api/profile', profile);
-router.use('/api/profile/contacts', contacts);
-router.use('/api/profile/send', send);
-router.use('/api/profile/messages', messages);
+router.use('/api/sign', sign)
+router.use('/api/profile', profile)
+router.use('/api/profile/contacts', contacts)
+router.use('/api/profile/send', send)
+router.use('/api/profile/messages', messages)
+router.use('/.well-known/webfinger', webfinger)
 
 module.exports = {
   path: '/',
