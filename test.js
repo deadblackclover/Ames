a096bcb5deb899b4be78dec0610f720089256a83
DEADBLACKCLOVER <deadblackclover@protonmail.com>
2018-03-22 21:30:34 +0700

    Add test logger and test db users

--- test.js
+++ test.js
@@ -1,5 +1,42 @@
 import test from 'ava'
+import fs from 'fs'
+import logger from './libs/logger'
+import db from './libs/db'
 
 test('empty test', t => {
   t.pass()
+})
+
+test.cb('logger', t => {
+  logger.save('test', 'test')
+
+  fs.readFileSync('./logs/test.csv', function(err, data) {
+    if (err) throw err
+    t.regex(data, /test/gi)
+  })
+  t.end()
+})
+
+test.cb('db users', t => {
+  db.users.insert({
+    uid: '0123456789',
+    username: 'username',
+    url: 'localhost',
+    fname: 'First',
+    lname: 'Last',
+    email: 'username@test',
+    photo: null,
+    token: 'token',
+    okey: '',
+    ckey: ''
+  }, function(err) {
+    t.fail(err)
+  })
+  
+  db.users.find({token:'token'}, function(err, docs) {
+    if(err) throw err
+    t.is(docs[0].uid,'0123456789')
+  })
+
+  t.end()
 })
