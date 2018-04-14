d264ed33007a9682465559dfd57c01fc17e574c8
DEADBLACKCLOVER <deadblackclover@protonmail.com>
2018-04-14 12:32:14 +0700

    Add style for search

--- components/search.vue
+++ components/search.vue
@@ -3,5 +3,10 @@
     <div class="name-blocks">
       Search
     </div>
+    <div class="search-block">
+      In developing
+    </div>
   </div>
 </template>
+
+<style lang="stylus" src="~/assets/css/search.styl"></style>
