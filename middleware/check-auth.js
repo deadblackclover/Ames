a1309dcc0d5778778fb03f08bcfba78586b1704a
DEADBLACKCLOVER <deadblackclover@protonmail.com>
2018-05-01 12:45:12 +0700

    Add copyright in file

--- middleware/check-auth.js
+++ middleware/check-auth.js
@@ -1,3 +1,7 @@
+/*   Copyright (c) 2018, Asocio. This file is
+ *   licensed under the GNU General Public License version 3 or later. See
+ *   the LICENSE file.
+ */
 export default function({ redirect, req }) {
   if (!req.session.authorized) {
     return redirect('/')
