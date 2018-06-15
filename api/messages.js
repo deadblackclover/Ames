18d3eeab6523b92051ec016c0051f59a2a501ae6
DEADBLACKCLOVER <deadblackclover@protonmail.com>
2018-06-16 04:23:10 +0700

    Sort message

--- api/messages.js
+++ api/messages.js
@@ -12,7 +12,7 @@ router.post('/', (req, res) => {
   if (req.session.authorized) {
     let uid = req.session.uid
     let user = req.body.user
-    db.messages.find({uid:uid, $or: [{to: user}, {from: user}]}, function(err, docs) {
+    db.messages.find({uid:uid, $or: [{to: user}, {from: user}]}).sort({ timestamp: 1 }).exec(function(err, docs) {
       if (err) { logger.save('dbMessages', err) }
       if (docs[0] !== undefined) {
         res.send(docs)
